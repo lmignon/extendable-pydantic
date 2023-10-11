92d7fd9f021603a5b2cb49f4e080264554342741
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-10-11 11:58:05 +0200

    eases import of StrictExtendableBaseModel from root module root

--- src/extendable_pydantic/__init__.py
+++ src/extendable_pydantic/__init__.py
@@ -3,4 +3,5 @@
 # shortcut to main used class
 from .main import ExtendableModelMeta
 from .models import ExtendableBaseModel
+from .models import StrictExtendableBaseModel
 from .version import __version__
